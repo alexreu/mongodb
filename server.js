401bf6aadc46214d14ce688a05aa2b24f5dcf809
alexreu <alexxxx.974@gmail.com>
2018-06-20 13:51:41 +0400

    petite mise en forme du html

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const app = express();
-let port = 3012;
-let bdd = require('mongodb').MongoClient;
-let assert = require('assert');
-let url = "mongodb://localhost:27017/alexandre";
+const port = 3012;
+const bdd = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = "mongodb://localhost:27017/alexandre";
 
 app.use(express.static('static'));
 
+// route pour acceder à la bdd et récupérer les données
 app.get('/bdd', function(req, res){
+
     bdd.connect(url, function (err, db) {
         assert.equal(null, err);
         console.log("connexion BDD Ok");
         let dbo = db.db("alexandre");
         dbo.collection("personnages").find({}).toArray(function (err, data) {
             console.log(data);
-            console.log(data[0].name);
             res.send(data);
         });
         db.close();
     });
-})
 
+});
 
+// renvoie la page html
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
@@ -29,4 +31,4 @@ app.get('/', function (req, res) {
 
 app.listen(port, function () {
     console.log('Server ON / port: ' + port);
-})
+});
--- static/main.js
+++ static/main.js
@@ -1,8 +1,9 @@
-let url = "http://localhost:3012/bdd";
+// requete ajax pour la récupération des données coté serveur
+const url = "http://localhost:3012/bdd";
 $.ajax({
     url: url,
     success: function (data) {
         console.log(data);
-        $('#data').append(data[0].name + "  " + data[0].genre);
+        $('#result').append('<div class="col-md-4 offset-md-4 mt-3" <p> Nom : ' + data[0].name + "  " + ' Genre : ' + data[0].genre + '</p></div>');
     }
 });
