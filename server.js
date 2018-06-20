0be227d4b975ad5c6efd9ab87d8f03b46df5fff6
alexreu <alexxxx.974@gmail.com>
2018-06-20 10:46:34 +0400

    creation de la route pour récuperer les données de la bdd

--- server.js
+++ server.js
@@ -5,15 +5,22 @@ let bdd = require('mongodb').MongoClient;
 let assert = require('assert');
 let url = "mongodb://localhost:27017/alexandre";
 
-bdd.connect(url, function (err, db) {
-    assert.equal(null, err);
-    console.log("connexion BDD Ok");
-    let dbo = db.db("alexandre");
-    dbo.collection("personnages").find({}).toArray(function (err, result) {
-        console.log(result);
+app.use(express.static('static'));
+
+app.get('/test', function(req, res){
+    bdd.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("connexion BDD Ok");
+        let dbo = db.db("alexandre");
+        dbo.collection("personnages").find({}).toArray(function (err, data) {
+            console.log(data);
+            console.log(data[0].name);
+            res.send(data);
+        });
+        db.close();
     });
-    db.close();
-});
+})
+
 
 app.get('/', function (req, res) {
     res.send('Hello World')
